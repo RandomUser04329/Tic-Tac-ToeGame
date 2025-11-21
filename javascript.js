b4e0e2408fefd9a453eeb3ecd12c62dc2dbe63bc
2sided <rsalinas24z@yahoo.com>
2025-11-20 20:25:12 -0600

    Beginning of the project

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+//Factory Function that creates a Player
+function createPlayer(name) {
+    const playerName = "0" + name; 
+    const playerLevel = 0;
+    return { playerName, playerLevel };
+}
+
+// 
+const ticTacToeGame = {
+    
+
+
+}
