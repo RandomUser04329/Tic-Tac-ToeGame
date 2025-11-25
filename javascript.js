674cbdb589e0eaedb16f6bee81d37f1bd37f574d
2sided <rsalinas24z@yahoo.com>
2025-11-24 19:52:40 -0600

    More function setup for the Game.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
 //the parent function that starts the WHOLE game
 function startGame(name) { 
-    let playerName = name;
-    let playerLevel = 0; 
+    let playerName;
+    let playerLevel;
     let playerColor;
+    let rounds; 
 
     let Player = {
-        playerName,
-        playerLevel,
+        playerName: name,
+        playerLevel: 0,
         playerColor,
+        rounds: parseInt(prompt("Enter the amount of rounds you want to play: "))
     };
 
     let computerAI = {
@@ -16,21 +18,52 @@ function startGame(name) {
         compColor: "Random"
     }
 
+    let userInfo = { 
+        Player, 
+        computerAI
+    };
+
+
+    if (userInfo.Player.rounds != 0 && userInfo.Player.playerName != "") {
+        return gameFunction(userInfo); 
+    } else { 
+        if (userInfo.Player.playerName === "") {
+            while (userInfo.Player.playerName === "") {
+                userInfo.Player.playerName = prompt("Please enter a name: ");
+             if (userInfo.Player.playerName != "") {
+                return gameFunction(userInfo); 
+             }
+            }
+        }
+        if (userInfo.Player.rounds === 0) {
+            while (userInfo.Player.rounds === 0) {
+                userInfo.Player.rounds = parseInt(prompt("Please enter the amount of rounds you want to play: "));
+             if (userInfo.Player.rounds != 0) {
+                return gameFunction(userInfo); 
+             }
+            }
+        }
+    }
+
+
+
+
+    function gameFunction(userInfo) { 
+        let gameRounds = userInfo.Player.rounds; 
+
+        let playerChoices = [gameRounds]; 
+        let computerChoices = [gameRounds]; 
+        let gameEnd = 0;
+
+        while (gameEnd <= gameRounds) {
+            let playerChoice = prompt(parseInt("Enter a number 1-6: "))
+        }
+
+    }
+
+
+
 }
 
-//Choose your rounds (child function)
-function chooseRounds(rounds, player, computer) {
-    let Rounds = rounds;
-
-    return ticTacToeGame(player, computer, Rounds);
-}
 
 
-
-//The Game (child function)
-function ticTacToeGame(Player, Computer, Rounds) {
-    console.log(Player);
-    console.log(Computer);
-
-
- }
